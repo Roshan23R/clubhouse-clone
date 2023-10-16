0832ed3454ed4fb1414e6af4ab8e3c353bdab737
Rkesh Roushan <rakeshroushan2341@gmail.com>
2023-10-16 19:09:29 +0530

    DailyInfoCard done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
+import DailyInfoCard from "../components/DailyInfoCard";
+import style from "../style/home.module.css";
 
-export default function Home(){
-    return (
-        <Header/>
-    )
-};
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className={style.home_container}>
+        <DailyInfoCard />
+      </div>
+    </>
+  );
+}
