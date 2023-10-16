a2cdc275a5873d66e2fc8abc9e0e6b411f7a8179
Rkesh Roushan <rakeshroushan2341@gmail.com>
2023-10-16 18:31:46 +0530

    App Layout Header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
+import Header from "../components/Header";
 
 export default function Home(){
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <Header/>
     )
 };
